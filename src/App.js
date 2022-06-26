6ec07e593ad0496271131b0f84241d70b65b252b
Oerd Bej <69147540+OerdBej@users.noreply.github.com>
2022-06-26 19:17:13 +0200

    Update App.js
    Updating CsS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import { useQueries } from "react-query";
+.App {
+  text-align: center;
+}
 
-const App = () => {
-  console.log(useQueries);
-  return <div>Hello World from React Query!</div>;
-};
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
 
-export default App;
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+.App-header {
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App-link {
+  color: #61dafb;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
